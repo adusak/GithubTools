624d306624af2ba5e592f519092ea2f55e5b23a4
Adam Melkus <adam@melkus.info>
2016-05-19 09:05:45 +0200

    intial commit

---
+++ stats.rb
@@ -0,0 +1,69 @@
+require 'json'
+require 'rest-client'
+require 'pp'
+require 'csv'
+
+URL = 'https://api.github.com/repos/rhomobile/rhodes/stats/contributors'.freeze
+
+response = RestClient.get URL
+json = JSON.parse(response.to_str)
+total = json.map { |e| e['total'] }.reduce(&:+)
+puts "Total of commits: #{total}"
+
+RHO_END = Time.new(2011, 10).to_i
+MOTO_START = Time.new(2011, 10).to_i
+MOTO_END = Time.new(2014, 4).to_i
+ZEBRA_START = Time.new(2014, 4).to_i
+ZEBRA_END = Time.new.to_i
+
+result = []
+charts = { rho: Hash.new(0), moto: Hash.new(0), zebra: Hash.new(0) }
+json.each do |o|
+  res_o = { name: o['author']['login'] }
+  weeks = o['weeks']
+  rho_weeks = weeks.select { |w_o| w_o['w'] < RHO_END }
+  moto_weeks = weeks.select { |w_o| w_o['w'] >= MOTO_START && w_o['w'] <= MOTO_END }
+  zebra_weeks = weeks.select { |w_o| w_o['w'] > ZEBRA_START }
+  res_o[:rho_weeks_total] = rho_weeks.map { |w_o| w_o['c'] }.reduce(&:+)
+  res_o[:moto_weeks_total] = moto_weeks.map { |w_o| w_o['c'] }.reduce(&:+)
+  res_o[:zebra_weeks_total] = zebra_weeks.map { |w_o| w_o['c'] }.reduce(&:+)
+  res_o[:total] = res_o[:rho_weeks_total] + res_o[:moto_weeks_total] + res_o[:zebra_weeks_total]
+  res_o[:total_real] = weeks.map { |w_o| w_o['c'] }.reduce(&:+)
+
+  rho_weeks.each do |w|
+    charts[:rho][w['w']] = charts[:rho][w['w']] + w['c']
+  end
+  moto_weeks.each do |w|
+    charts[:moto][w['w']] = charts[:moto][w['w']] + w['c']
+  end
+  zebra_weeks.each do |w|
+    charts[:zebra][w['w']] = charts[:zebra][w['w']] + w['c']
+  end
+
+  result << res_o
+end
+
+sorted = result.sort_by { |e| e[:total] }.reverse.take(10)
+
+puts "\nALL THE CONTRIBUTORS\n"
+pp sorted
+
+def group_by_months(weeks)
+  l_res = Hash.new(0)
+  weeks.each do |d, c|
+    time = Time.at(d)
+    l_res[Time.new(time.year, time.month, 1)] = l_res[Time.new(time.year, time.month, 1)] + c
+  end
+
+  l_res
+end
+
+pp charts
+
+charts.each do |k, v|
+  File.open("#{k}_chart.csv", 'wb') do |file|
+    h = group_by_months(v)
+    file << 'date,commits'
+    h.each { |d, c| file << "\n#{d.strftime('%Y-%m-%d')},#{c}" }
+  end
+end
